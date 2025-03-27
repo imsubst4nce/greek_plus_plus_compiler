f3bbfa3f7bc232c7a7175a2e45c74cd649ce7a80
imsubst4nce <nickolis21@gmail.com>
2025-03-27 18:30:54 +0200

    Changes to quad.py and notes

--- Our-project/quad.py
+++ Our-project/quad.py
@@ -1,25 +1,38 @@
 # KLASH QUAD
 class Quad:
     def __init__(self):
-        print("Initializing Quad class...")
-        self.lastTemp = "T_0" # last temp variable inits to T_0
-        self.label = None # label will be given by calling backpatch method
+        # probably to be moved in genQuad
+        # self.operation = operation # e.g ('+', a, 1 , a)
+        # self.operand1 = operand1
+        # self.operand2 = operand2
+        # self.operand3 = operand3
+        # self.label = None # label will be given by calling backpatch method
+        self.quadlist = [] # init quads list
+        self.tempvar = "T_0" # last temp variable inits to T_0
+    
+    def __str__(self):
+        print(f'({self.operation},{self.operand1},{self.operand2},{self.operand3})')
 
     # Creates a new quad
-    def genQuad(self, operator, operand1, operand2, operand3):
-        self.operator = operator
+    def genQuad(self, operation, operand1, operand2, operand3):
+        self.operation = operation # e.g ('+', a, 1 , a)
         self.operand1 = operand1
         self.operand2 = operand2
         self.operand3 = operand3
+        self.label = None # init quad label is None and will be replaced during backpatching
+        self.quadlist.append([operation, operand1, operand2, operand3, self.label])
     
     # Returns next quad's label
+    # Needs to be fixed!!!
     def nextQuad(self):
-        return
+        if not self.quadlist:
+            return 0
+        return # if there are quads what does it return?
     
     # Next temp variable name is created and returned
     def newTemp(self):
-        newTemp = "T_" + str(int(self.lastTemp[2])+1)
-        self.lastTemp = newTemp
+        newTemp = "T_" + str(int(self.tempvar[2])+1)
+        self.tempvar = newTemp
         return newTemp
 
     # Returns an empty list which later on
@@ -37,10 +50,15 @@ class Quad:
     def mergeLists(self, list1, list2):
         return list1 + list2
 
-    # Iterates over the quads from the list
-    # and if it finds the quad under the given label,
-    # 
+    # Replaces empty quad labels with the 
+    # list: contains the indices where all the 'None' labeled quads are
+    # label: the label that will replace 'None'
     def backpatch(self, list, label):
+        for index in list:
+            self.quadlist[index].label = label
+        
+        list.clear() # once finished deallocate memory
+
     
 if __name__ == "__main__":
-    quad = Quad()
+    newquad = Quad()
