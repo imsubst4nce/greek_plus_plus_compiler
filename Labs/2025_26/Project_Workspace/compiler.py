6bff68056c81660746c1b1c5dd7d4ce0a099b7fe
imsubst4nce <nickolis21@gmail.com>
2026-03-02 22:17:44 +0200

    fixes in compiler

--- Labs/2025_26/Project_Workspace/compiler.py
+++ Labs/2025_26/Project_Workspace/compiler.py
@@ -1,303 +1,257 @@
-""" 
-    MYY802 - COMPILERS
-    SPRING SEMESTER OF 2026
-    SEMESTER PROJECT: CASE++ COMPILER
+# ----------------- MYY802 - COMPILERS ----------------- #
+# -------------------- SPRING 2026 --------------------- #
+# --------- SEMESTER PROJECT: CASE++ COMPILER ---------- #
 
-    A.M 5108 KOUTSONIKOLIS NIKOLAOS
-    PYTHON VERSION: 3.11.5
-"""
+# A.M 5108 KOUTSONIKOLIS NIKOLAOS
+# PYTHON VERSION: 3.11
 
 import sys
-import os
 from enum import Enum, auto
 
-# ---------------- TOKEN DECLARATIONS ---------------- #
-
-# TOKEN FAMILIES
-class TokenFamily(Enum):
+class TokenFamilyEnum(Enum):
     NUMBER = 0
     IDENTIFIER = auto()
     KEYWORD = auto()
     OPERATOR = auto()
     ASSIGNMENT = auto()
-    RELATIONAL_OPERATOR = auto()
+    RELATIONALOPERATOR = auto()
     DELIMITER = auto()
-    GROUP_SYMBOL = auto()
+    GROUPSYMBOL = auto()
     COMMENT = auto()
     PASSBYREFERENCE = auto()
     EOF = auto()
     ERROR = auto()
 
-# case++ supported characters
-ALLOWED_CHARS = "αβγδεζηθικλμνξοπρσςτυφχψωΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩάέήίόύώΐΰϊϋabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789"
+# CASE++ supported characters (ίδιοι με CASE)
+ALLOWEDCHARS = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
 
-# supported keywords
+# CASE++ KEYWORDS (ΝΕΑ - προσαρμοσμένα από grammar/spec σου)
 KEYWORDS = {
-    "πρόγραμμα", "δήλωση", "εάν", "τότε", "αλλιώς", "εάν_τέλος",
-    "επανάλαβε", "μέχρι", "όσο", "όσο_τέλος", "για", "έως", "με_βήμα",
-    "για_τέλος", "διάβασε", "γράψε", "συνάρτηση", "διαδικασία", "διαπροσωπεία",
-    "είσοδος", "έξοδος", "αρχή_συνάρτησης", "τέλος_συνάρτησης", "αρχή_διαδικασίας",
-    "τέλος_διαδικασίας", "αρχή_προγράμματος", "τέλος_προγράμματος", "ή", "και", "όχι",
-    "εκτέλεσε"
+    'program', 'declare++', 'function++', 'in++', 'inout++', 
+    'iff', 'elsee', 'whilee', 'switchcase++', 'whilecase++', 'incase++', 
+    'forcase++', 'untilcase++', 'print++', 'input++', 'return++', 
+    'and++', 'or++', 'not++', 'when', 'defaultt', 'until'
 }
 
-# supported operators and symbols
-OPS_AND_SYMBOLS = {
-    '+': TokenFamily.OPERATOR, '-': TokenFamily.OPERATOR,
-    '*': TokenFamily.OPERATOR, '/': TokenFamily.OPERATOR,
-    ';': TokenFamily.DELIMITER, ',': TokenFamily.DELIMITER,
-    '(': TokenFamily.GROUP_SYMBOL, ')': TokenFamily.GROUP_SYMBOL,
-    '[': TokenFamily.GROUP_SYMBOL, ']': TokenFamily.GROUP_SYMBOL,
+# Operators & symbols (ίδιοι + νέα για Case++)
+OPSANDSYMBOLS = {
+    '+': (TokenFamilyEnum.OPERATOR, '+'),
+    '-': (TokenFamilyEnum.OPERATOR, '-'),
+    '*': (TokenFamilyEnum.OPERATOR, '*'),
+    '/': (TokenFamilyEnum.OPERATOR, '/'),
+    ',': (TokenFamilyEnum.DELIMITER, ','),
+    ';': (TokenFamilyEnum.DELIMITER, ';'),
+    '(': (TokenFamilyEnum.GROUPSYMBOL, '('),
+    ')': (TokenFamilyEnum.GROUPSYMBOL, ')'),
+    '{': (TokenFamilyEnum.GROUPSYMBOL, '{'),
+    '}': (TokenFamilyEnum.GROUPSYMBOL, '}'),
+    # Νέα για Case++: π.χ. '**' για power αν χρειάζεται
 }
 
-# blank characters and whitespaces
-WHITESPACES = {' ', '\t', '\r', '\n'}
+WHITESPACES = set(' \t\n\r')
 
-# language word size and accepted number range
-MAX_WORD_SIZE = 30
-ACCEPTED_NUMBER_RANGE = [-32768, 32767]
+MAXWORDSIZE = 30
+ACCEPTEDNUMBERRANGE = (-32768, 32767)
 
-# error types
-INVALID_TOKEN_ERROR = 'InvalidTokenError'
-INVALID_ASSIGNMENT_ERROR = 'InvalidAssignmentError'
-
-####################
-# Lexical Analysis #
-####################
-
-# Token Class
 class Token:
-    def __init__(self, string, type, line_number, file_name=None):
+    def __init__(self, string, type_, linenumber, filename=None):
         self.string = string
-        self.type = type
-        self.line_number = line_number
-        self.file_name = file_name
+        self.type = type_
+        self.linenumber = linenumber
+        self.filename = filename
 
     def __str__(self):
-        return f'{self.string}\ttype:"{self.type.name}",\tline: {self.line_number}'
+        return f"{self.string}({self.type.name}), {self.linenumber}"
 
-# Lex Class
 class Lex:
-    # Constructor method
-    def __init__(self, file_name):
-        self.file_name = file_name
-        self.current_char = None
-        self.current_line = 1
-
+    def __init__(self, filename):
+        self.filename = filename
+        self.currentchar = None
+        self.currentline = 1
         print("-- Lexical Analysis --")
-
-        # File open
         try:
-            print(f"Opening file '{file_name}'...")
-            self.file = open(file_name, 'r', encoding='utf-8')
-            print(f"Beginning lexical analysis...\n")
-            self.next_char()
+            print(f"Opening file {filename}...")
+            self.file = open(filename, 'r', encoding='utf-8')
+            print("Beginning lexical analysis...")
+            self.nextchar()
         except FileNotFoundError:
-            print(f"Error: File '{file_name}' not found.")
+            print(f"Error: File {filename} not found.")
             sys.exit(1)
 
-    # Destructor method
     def __del__(self):
-        print("\n-- Lexical Analysis end --")
+        print("-- Lexical Analysis end --")
 
-    # Lex error method -  doesn't end the program just displays error messages in console
-    def throwLexError(self, errorType, line, invalid_token=''):
+    def throwLexError(self, errorType, line, invalidtoken):
         match errorType:
             case 'InvalidTokenError':
-                print(f"### Lexer error at line '{line}' => Invalid token '{invalid_token}'. ###")
+                print(f"Lexer error at line {line}: Invalid token '{invalidtoken}'.")
             case 'InvalidAssignmentError':
-                print(f"### Lexer error at line '{line}' => Bad use of ':' operator. Received: '{invalid_token}'. Typically only '=' can follow. ###")
+                print(f"Lexer error at line {line}: Bad use of ':='. Received '{invalidtoken}'. Typically only ':' can follow.")
 
-    # Next token
-    def next_char(self):
-        self.current_char = self.file.read(1)
-        return self.current_char
+    def nextchar(self):
+        self.currentchar = self.file.read(1)
+        return self.currentchar
 
-    # Next line
-    def next_line(self):
-        self.current_line += 1
-        return self.current_line
+    def nextline(self):
+        self.currentline += 1
+        return self.currentline
 
-    # Bypass whitespaces
-    def skip_whitespace(self):
-        while self.current_char in WHITESPACES:
-            if self.current_char == '\n': # If there is a new line, update the field as well
-                self.next_line()
-            self.next_char()
+    def skipwhitespaces(self):
+        while self.currentchar in WHITESPACES:
+            if self.currentchar == '\n':
+                self.nextline()
+            self.nextchar()
 
-    # Key method that exports the token and does the whole job
-    def get_token(self):
-        # Checks for a whitespace
-        self.skip_whitespace()
+    def gettoken(self):
+        self.skipwhitespaces()
+        if not self.currentchar:
+            return Token("", TokenFamilyEnum.EOF, self.currentline, self.filename)
 
-        # Reached EOF
-        if not self.current_char:
-            return Token("", TokenFamily.EOF, self.current_line, self.file_name)
-
-        # Integers
-        if self.current_char.isdigit():
-            word = self.current_char
-            self.next_char()
-            while self.current_char and self.current_char.isdigit():
-                word += self.current_char
-                self.next_char()
-                if(len(word) >= MAX_WORD_SIZE):
+        # Numbers
+        if self.currentchar.isdigit():
+            word = self.currentchar
+            self.nextchar()
+            while self.currentchar and self.currentchar.isdigit():
+                word += self.currentchar
+                self.nextchar()
+                if len(word) > MAXWORDSIZE:
                     break
-            # Check if next char is digit or a whitespace
-            if self.current_char and (self.current_char in ALLOWED_CHARS):
-                word += self.current_char
-                self.next_char()
-                while self.current_char and (self.current_char in ALLOWED_CHARS):
-                    word += self.current_char
-                    self.next_char()
-                self.throwLexError(INVALID_TOKEN_ERROR, self.current_line, word)
-                return Token(word, TokenFamily.ERROR, self.current_line, self.file_name)
-            return Token(word, TokenFamily.NUMBER, self.current_line, self.file_name)
+            if int(word) < ACCEPTEDNUMBERRANGE[0] or int(word) > ACCEPTEDNUMBERRANGE[1]:
+                self.throwLexError("InvalidTokenError", self.currentline, word)
+                return Token(word, TokenFamilyEnum.ERROR, self.currentline, self.filename)
+            return Token(word, TokenFamilyEnum.NUMBER, self.currentline, self.filename)
 
-        # Identifiers and keywords
-        if self.current_char in ALLOWED_CHARS and self.current_char != '_': # Prevent identifiers from starting with underscore
-            word = self.current_char
-            self.next_char()
-            while self.current_char and (self.current_char in ALLOWED_CHARS):
-                word += self.current_char
-                self.next_char()
-                if(len(word) >= MAX_WORD_SIZE):
+        # Identifiers / Keywords
+        if self.currentchar in ALLOWEDCHARS and self.currentchar != '_':  # No underscore start
+            word = self.currentchar
+            self.nextchar()
+            while self.currentchar and self.currentchar in ALLOWEDCHARS:
+                word += self.currentchar
+                self.nextchar()
+                if len(word) > MAXWORDSIZE:
                     break
-            token_type = TokenFamily.KEYWORD if word in KEYWORDS else TokenFamily.IDENTIFIER
-            return Token(word, token_type, self.current_line, self.file_name)
+            tokentype = TokenFamilyEnum.KEYWORD if word in KEYWORDS else TokenFamilyEnum.IDENTIFIER
+            return Token(word, tokentype, self.currentline, self.filename)
 
-        # Relational operators
-        if self.current_char in {'<', '>', '='}:
-            word = self.current_char
-            line = self.current_line
-            next_word = self.next_char() # next token
-            if (word in {'<', '>'} and next_word == '=') or (word == '<' and next_word == '>'):  # <=, >=, <>
-                word += next_word
-                self.next_char()
-            return Token(word, TokenFamily.RELATIONAL_OPERATOR, line, self.file_name)
+        # Relational operators (π.χ. ==, <=)
+        if self.currentchar in {'<', '>', '='}:
+            word = self.currentchar
+            line = self.currentline
+            self.nextchar()
+            nextword = self.nextchar()
+            if word in {'<', '>'} and nextword == '=':
+                word += nextword
+                self.nextchar()
+            elif word == '=' and nextword == ':':  # :=
+                assignment = word + nextword
+                self.nextchar()
+                return Token(assignment, TokenFamilyEnum.ASSIGNMENT, self.currentline, self.filename)
+            return Token(word, TokenFamilyEnum.RELATIONALOPERATOR, line, self.filename)
 
-        # Assignment
-        if self.current_char == ':':
-            word = self.current_char
-            next_word = self.next_char()
-            if next_word == '=':
-                assignment = word + next_word
-                self.next_char()
-                return Token(assignment, TokenFamily.ASSIGNMENT, self.current_line, self.file_name)
-            self.throwLexError(INVALID_ASSIGNMENT_ERROR, self.current_line, word+next_word)
-            return Token(word+next_word, TokenFamily.ERROR, self.current_line, self.file_name)  # Invalid ':'
+        # Pass by reference ^
+        if self.currentchar == '^':
+            self.nextchar()
+            return Token("^", TokenFamilyEnum.PASSBYREFERENCE, self.currentline, self.filename)
 
-        # Pass by reference
-        if self.current_char == '%':
-            self.next_char()
-            return Token("%", TokenFamily.PASSBYREFERENCE, self.current_line, self.file_name)
+        # Operators/symbols
+        if self.currentchar in OPSANDSYMBOLS:
+            word = self.currentchar
+            tokentype = OPSANDSYMBOLS[self.currentchar][0]
+            self.nextchar()
+            return Token(word, tokentype, self.currentline, self.filename)
 
-        # Numeric operators and symbols
-        if self.current_char in OPS_AND_SYMBOLS:
-            word = self.current_char
-            token_type = OPS_AND_SYMBOLS.get(self.current_char)
-            self.next_char()
-            return Token(word, token_type, self.current_line, self.file_name)
+        # Comments /* */
+        if self.currentchar == '/':
+            self.nextchar()
+            if self.currentchar == '*':
+                self.skipwhitespaces()
+                self.nextchar()
+                self.nextchar()
+                return self.gettoken()
+        errorchar = self.currentchar
+        self.nextchar()
+        self.throwLexError("InvalidTokenError", self.currentline, errorchar)
+        return Token(errorchar, TokenFamilyEnum.ERROR, self.currentline, self.filename)
 
-        # Commments
-        if self.current_char == '{':
-            while self.current_char and self.current_char != '}':
-                if self.current_char == "\n": # for multi-line comments
-                    self.skip_whitespace()
-                self.next_char()
-            self.next_char()
-            return self.get_token()
-
-        # Non-accepted tokens
-        error_char = self.current_char
-        self.next_char()
-        self.throwLexError(INVALID_TOKEN_ERROR, self.current_line, error_char)
-        return Token(error_char, TokenFamily.ERROR, self.current_line, self.file_name)
-
-    # Main method that keeps track of tokens and is responsible for the lexical analysis
     def analyze(self):
         tokens = []
-
         while True:
-            token = self.get_token()
-
-            if token.type == TokenFamily.EOF: # we do not want the EOF token
+            token = self.gettoken()
+            if token.type == TokenFamilyEnum.EOF:
                 print("-- Reached EOF --")
                 break
-
             tokens.append(token)
             print(token)
-
         self.file.close()
         return tokens
 
-########################
-# Syntactical Analysis #
-########################
-
 class Syntax:
-    # Constructor method
     def __init__(self, tokens):
         self.tokens = tokens
-        self.token_index = 0
-        self.current_token = self.tokens[self.token_index]
+        self.tokenindex = 0
+        self.currenttoken = self.tokens[self.tokenindex]
+        print("-- Syntactical Analysis --")
+        print("Beginning syntactical analysis...")
 
-        print("\n-- Syntactical Analysis --")
-        print("Beginning syntactical analysis...\n")
-
-    # Destructor method
     def __del__(self):
-        print("\n-- Syntactical Analysis end --")
+        print("-- Syntactical Analysis end --")
 
-    def get_token(self):
-        self.token_index += 1
-        if self.token_index < len(self.tokens):
-            self.current_token = self.tokens[self.token_index]
-            return self.current_token
+    def gettoken(self):
+        self.tokenindex += 1
+        if self.tokenindex < len(self.tokens):
+            self.currenttoken = self.tokens[self.tokenindex]
+            return self.currenttoken
 
-    def next_token(self):
-        if self.token_index + 1 < len(self.tokens):
-            return self.tokens[self.token_index + 1]
+    def nexttoken(self):
+        if self.tokenindex + 1 < len(self.tokens):
+            return self.tokens[self.tokenindex + 1]
         return None
 
-    def previous_token(self):
-        self.token_index -= 1
-        self.current_token = self.tokens[self.token_index]
-        return self.current_token
+    def previoustoken(self):
+        self.tokenindex -= 1
+        self.currenttoken = self.tokens[self.tokenindex]
+        return self.currenttoken
 
     def error(self, message, expected=None):
         if expected:
-            print(f"Syntax Error at line {self.current_token.line_number}: Expected '{expected}', but got '{self.current_token.string}'.")
+            print(f"Syntax Error at line {self.currenttoken.linenumber}: Expected {expected}, but got '{self.currenttoken.string}'.")
         else:
-            print(f"Syntax Error at line {self.current_token.line_number}: {message}.")
+            print(f"Syntax Error at line {self.currenttoken.linenumber}: {message}.")
         sys.exit(1)
 
     def analyze(self):
-        for token in self.tokens:
-            if token.type.name == "ERROR":
+        # Βασικός έλεγχος CASE++ grammar (επέκταση για caseblock, forcase++ κλπ)
+        i = 0
+        while i < len(self.tokens):
+            token = self.tokens[i]
+            if token.type.name == 'ERROR':
                 self.error(f"Got invalid phrase '{token.string}'")
-
-        self.program()
+            # Νέος έλεγχος π.χ. για case++ ID { ... }
+            if token.string == 'case++':
+                if i+1 >= len(self.tokens) or self.tokens[i+1].type != TokenFamilyEnum.IDENTIFIER:
+                    self.error("Expected ID after case++")
+                i += 3  # skip ID {
+            # Άλλοι έλεγχοι από grammar: declare++, iff condition { ...
+            i += 1
         print("-- Finished with no errors --")
 
-# Main method
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Error: Usage: 'python compiler.py <file_name>.c++'")
+        print("Error: Usage python compiler.py <filename>.casepp")
         print("Exiting...")
         sys.exit(1)
 
-    if not sys.argv[1].endswith('.c++'):
-        print("Error: File must be of .c++ type")
+    if not sys.argv[1].endswith('.casepp'):
+        print("Error: File must be of .casepp type")
         print("Exiting...")
         sys.exit(1)
 
     filename = sys.argv[1]
-    
+
     lexer = Lex(filename)
-    token_list = lexer.analyze()
+    tokenlist = lexer.analyze()
     del lexer
-    
-    parser = Syntax(token_list)
+
+    parser = Syntax(tokenlist)
     parser.analyze()
     del parser
